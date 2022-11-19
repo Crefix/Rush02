286265b9aba9f4cdbdbf82a55e04131d0f56e6d5
Cris Navas Marcano <crnavas-@c2r11s3.42madrid.com>
2022-11-19 18:45:02 +0100

    Sat 19 Nov 2022 18:45:02 CET

---
+++ ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crnavas- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 20:59:53 by crnavas-          #+#    #+#             */
+/*   Updated: 2022/11/05 16:17:05 by crnavas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(char *str)
+{
+	int	num;
+	int	sign;
+
+	num = 0;
+	sign = 1;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		num = (*str - '0') + (num * 10);
+		str++;
+	}
+	return (num * sign);
+}
+/*
+int	main(void)
+{
+	char	org[];
+
+	org[] = "-325632167";
+	//printf("Original: %d\n", atoi(org));
+	printf("Custom: %d\n", ft_atoi(org));
+}
+*/
